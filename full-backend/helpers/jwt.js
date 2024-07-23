93a75a51034d4e71bb6aed373a7a0c1906bdc6d0
niallmc1995 <niallmcaffrey@gmail.com>
2024-07-23 21:22:17 +0100

    fixing backend
    removing depreciated packages and fixing jwt.js

--- full-backend/helpers/jwt.js
+++ full-backend/helpers/jwt.js
@@ -1,18 +1,19 @@
-const expressJwt = require('express-jwt');
+const { expressjwt: jwt } = require('express-jwt');
+
 
 function authJwt() {
     const secret = process.env.secret;
     const api = process.env.API_URL;
-    return expressJwt({
+    return jwt({
         secret,
         algorithms: ['HS256'],
         isRevoked: isRevoked
     }).unless({
         path: [
-             {url: /\/public\/uploads(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
-            {url: /\/api\/v1\/orders(.*)/ , methods: ['POST', 'OPTIONS'] },
+            { url: /\/public\/uploads(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
+            { url: /\/api\/v1\/orders(.*)/, methods: ['POST', 'OPTIONS'] },
             `${api}/users/login`,
             `${api}/users/register`,
         ]
